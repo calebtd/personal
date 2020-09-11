1966022c8f5ccfbf3037d275a2eb20ad19182441
Caleb (School) <dillecal000@alpinesd.org>
2020-09-11 08:51:23 -0600

    Class work

--- B5/B5_fishingRegulations.py
+++ B5/B5_fishingRegulations.py
@@ -3,10 +3,21 @@
 # Fishing Regulations
 # I did not copy anyone but Mr. Blair
 
+# Guidebook link: wildlife.utah.gov/guidebooks/2020_fishing_guidebook.pdf
+
 # Ask where, give options
 where = input("Where were you fishing? (Deer Creek, Lake Powell, Green River): ").lower
-
-
+species = input("What species of fish did you catch?: ")
 length = float(input("How long is the fish?: "))
 
-print(length)
+if where == "deer creek":
+    print(where)
+
+if where == "lake powell":
+    print(where)
+
+if where == "green river":
+    print(where)
+
+else:
+    print("Not an option. Try again.")

15d0f868e63319dc3eaf471099ccc336e660efa5
Caleb <caleb@dillenbeck.org>
2020-09-17 01:33:59 -0600

    Start Captain Jack

---
+++ B5/B5_captainJack.py
@@ -0,0 +1,11 @@
+# Caleb Dillenbeck
+# B5 Programming 1 - Mr. Blair
+# Captain Jack Assignment
+# I did not copy anyone. This is my original work
+
+# import math
+
+# print backstory
+
+input("How many crew members?: ")
+input("How many coins?: ")

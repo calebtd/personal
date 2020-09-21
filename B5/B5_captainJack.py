3ec6cb9c3e4eefd968f1c72cce239d2ff5b15637
Caleb <caleb@dillenbeck.org>
2020-09-21 07:44:40 -0600

    Captain Jack

--- B5/B5_captainJack.py
+++ B5/B5_captainJack.py
@@ -7,11 +7,38 @@
 
 # print backstory
 
-crew = int(input("How many crew members? (including you and the first mate): "))
-coins = int(input("How many coins?: "))
+print("\nYour name is Captain Jack. You are the captain of the Black Pearl.\n"
+      "You and your crew have had a successful few months plundering and finding treasure,\n"
+      "and you're all ready for a break. You finally pull up to the dock and see...")
+input("(Press Enter to continue...)")
+
+print(""
+      " __          __  _                             _        \n"
+      " \ \        / / | |                           | |       \n"
+      "  \ \  /\  / /__| | ___ ___  _ __ ___   ___   | |_ ___  \n"
+      "   \ \/  \/ / _ \ |/ __/ _ \| '_ ` _ \ / _ \  | __/ _ \ \n"
+      "    \  /\  /  __/ | (_| (_) | | | | | |  __/  | || (_) |\n"
+      "     \/  \/_\___|_|\___\___/|_| |_| |_|\___|   \__\___/ \n"
+      "         |__   __|       | |                            \n"
+      "   "
+      "         | | ___  _ __| |_ _   _  __ _  __ _         \n"
+      "            | |/ _ \| '__| __| | | |/ _` |/ _` |        \n"
+      "            | | (_) | |  | |_| |_| | (_| | (_| |        \n"
+      "            |_|\___/|_|   \__|\__,_|\__, |\__,_|        \n"
+      "                                     __/ |              \n"
+      "                                    |___/")
+
+print("You address your crew and say: '...Here's three coins for each of you, \n"
+      "go into town and have some fun. We'll figure out how to best divvy up our shares'")
+
+crew = int(input("How many crew members are there? (including you and your first mate): "))
+coins = int(input("How many coins are there?: "))
+coins -= ((crew - 2) * 3)
 
 # go into town
+print("You and your first mate stay behind, and decide to take some extra for yourselves.\n"
+      "You take 12% and you agree to give him 8%")
 
-coins = coins - ((crew - 2) * 3)
+coins -= 
 
 print(coins)

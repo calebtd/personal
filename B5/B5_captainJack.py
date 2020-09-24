b6dcd8a851c9036c63338c7caebcb2508bb7f689
Caleb <caleb@dillenbeck.org>
2020-09-24 01:06:33 -0600

    Jack Done

--- B5/B5_captainJack.py
+++ B5/B5_captainJack.py
@@ -4,10 +4,19 @@
 # I did not copy anyone. This is my original work
 
 
-# print backstory
+# Start with id
 print("\nYour name is Captain Jack. You are the captain of the Black Pearl.\n"
-      "You and your crew have had a successful few months plundering and finding treasure,\n"
-      "and you're all ready for a break. You finally pull up to the dock and see...")
+      "You and your crew have had a successful few months plundering and \n"
+      "finding treasure, and you're all ready for a break.\n")
+
+# Ask how many crew and coins, assign to variables
+crew = int(input("How many crew members do you have? (including you and your first mate): "))
+coins = int(input("How many coins did you get?: "))
+
+# Print backstory
+print("\nYou and your crew finally pull up to the dock and see:")
+
+# wait instead of displaying everything at once
 input("(Press Enter to continue...)")
 
 # ASCII Art just for fun
@@ -31,29 +40,55 @@ print(""
 print("You address your crew and say: 'A good season we've 'ad, ey?\n"
       "Plenty a' gold for all of us. Now who's up for some fun in \n"
       "Tortuga! 'Ere's three coins for each o' you, and shove off. \n"
-      "Gibbs and me 'ill decide 'ow best to divvy up our shares'\n")
+      "Gibbs and me 'ill decide 'ow best to divvy up our shares.'")
 
-# Ask how many crew and coins, assign to variables
-crew = int(input("How many crew members are there? (including you and your first mate): "))
-coins = int(input("How many coins are there?: "))
+# wait instead of displaying everything at once
+input("(Press Enter)")
 
 # Pull out 3 coins for each crew member, except Jack and Gibbs
-coins -= ((crew - 2) * 3)
+coins -= (crew - 2) * 3
 
 # sneaky sneaky
-print("\nYou and your first mate stay behind, both wanting a bit of extra gold. \n"
-      "You sneak in and take 12%, without Gibbs knowing. Then when you are \n"
-      "counting it up, you agree to give Gibbs 8% for being a good first mate.")
+print("\nYou and your first mate stay behind, both wanting a bit of extra gold.\n"
+      "You sneak in and take 12%, without Gibbs knowing. Later, when you and Gibbs\n"
+      "are counting the coins up, you decide to give Gibbs 8% for being a good first mate.")
 
-coins //= (25/22)  # 12%
-captainJack = coins
+# wait instead of displaying everything at once
+input("(Press Enter)")
 
-coins *= .92  # 8%
-gibbs = coins
+# pull 12% for Jack
+jack = int(coins * .12)  # int so there's no decimals
+coins -= jack  # shortcut for coins - jack
 
-print("\nYour crew returns, and you then divide the remaining gold to everyone, \n"
-      "including Gibbs and yourself.")
+# pull for Gibbs, same as Jack but 8%
+gibbs = int(coins * .08)
+coins -= gibbs
 
+# divide evenly
+print("\nYour crew returns, so you then divide the remaining gold to everyone, \n"
+      "including Gibbs and yourself.\n"
+      "Any remaining coins go to the Pirate's Benevolent Association.")
 
-print(coins // crew)
-print(coins % crew)
+# use // for just int division
+split = coins // crew
+# anything after goes to PBA
+coins -= split * crew
+
+# split to Jack and Gibbs too
+jack += split
+gibbs += split
+
+print("\nHere's how many gold coins everyone gets:")
+
+# wait instead of displaying everything at once
+input("(Press Enter)")
+
+# print a nice looking table
+print("\n┌─────────────────────┐")
+table = {'Jack': jack,
+         'Gibbs': gibbs,
+         'Crew': split,
+         'PBA': coins}
+for key, value in table.items():
+    print(f'│ {key:6}  -  {value:8} │')
+print("└─────────────────────┘")
--- test.py
+++ test.py
@@ -2,21 +2,31 @@ coins = 1000
 crew = 20
 PBA = 0
 
-coins -= 18 * 3
-print(coins)
+coins -= (crew - 2) * 3
 
-jack = coins // (25/3)
-coins //= 25/22
-PBA += coins % (25/22)
+jack = int(coins * .12)
+coins -= jack
 
-gibbs = coins // (25/2)
-coins //= (25/23)
-PBA += coins % (25/23)
+gibbs = int(coins * .08)
+coins -= gibbs
 
-PBA += coins % crew
 split = coins // crew
+coins -= split * crew
+
+jack += split
+gibbs += split
 
 print(jack)
 print(gibbs)
 print(split)
-print(PBA)
+print(coins)
+
+print("\n┌─────────────────────┐")
+table = {'Jack': jack,
+         'Gibbs': gibbs,
+         'Crew': split,
+         'PBA': coins}
+for key, value in table.items():
+    print(f'│ {key:6}  -  {value:8} │')
+print("└─────────────────────┘")
+

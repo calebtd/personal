a5722c30980c8e360a0f6a86243470e8a57b40bf
Caleb <dillecal000@alpinesd.org>
2020-10-23 08:51:27 -0600

    In Class 10/23

--- B5/B5.8_randomNumbers.py
+++ B5/B5.8_randomNumbers.py
@@ -1,7 +1,11 @@
 # Caleb Dillenbeck
 # B5 Programming 1 - Mr. Blair
-# Common ACT Math Problems Assignment
+# Random Numbers Assignment
 # I did not copy anyone
+
+# ─────────
+# PROBLEM 1
+# ─────────
 
 import math
 import random
@@ -12,10 +16,18 @@ y = 0
 t = turtle
 t.speed(5000)
 
-while True:
+for loop in range(1000):
 
     angle = random.random() * 2 * math.pi
     x += math.cos(angle) * 2
     y += math.sin(angle) * 2
 
     t.goto(x, y)
+
+dist = (x - 0) + (y - 0)
+print(f"The distance between the starting point\n"
+      f"and the ending point is {dist:.4f}")
+
+# ─────────
+# PROBLEM 2
+# ─────────
---
+++ B5/B5.other_vendingMachineExample.py
@@ -0,0 +1,45 @@
+# Caleb Dillenbeck
+# B5 Programming 1 - Mr. Blair
+# Vending Machine Assignment
+# I did not copy anyone
+
+# it can hold 2000 bags of doritos
+
+# TODO
+# welcome
+# power on
+# insert coin
+# total coins
+# total bags
+# one bag is $0.50
+# PARTICIPATION POINTS
+#
+
+# put in while True
+
+# welcome
+print("Welcome to my vending machine. We sell doritos. Lot's of them! Enter $0.50 to buy.")
+
+# set state variables
+power = False
+totalCoinsInMachine = 0
+coinTowardPurchase = 0
+bagsOfDoritos = 2000
+
+# ask user for command
+command = input("Enter Command: ").lower()
+
+if command == "power on":
+    # power checker
+
+elif command == "insert coin":
+    # power check
+    # add coins
+    # if enough coins, spit out bag
+    # print coins inserted
+
+elif command == "total coins":
+    # print coins
+
+elif command == "total bags":
+    # print bags

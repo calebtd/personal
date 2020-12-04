2bdfa90978f9221a8cb34b29e522251930c8bc47
Caleb <dillecal000@alpinesd.org>
2020-12-04 08:50:55 -0700

    In Class 12/4

--- B5/B5.14_definingFunctions.py
+++ B5/B5.14_definingFunctions.py
@@ -2,3 +2,34 @@
 # B5 Programming 1 - Mr. Blair
 # Defining Functions
 # I did not copy anyone
+
+# 1.
+
+# 2.
+
+# 3.
+
+# 4.
+
+# 5.
+
+# 6.
+def triangle_area(a, b, c):
+    return (a + b + c)/2
+
+
+while True:
+    try:
+        x = int(input('First side length: '))
+        y = int(input('Second side length: '))
+        z = int(input('Third side length: '))
+        break
+    except ValueError:
+        print("Please type valid numbers.\n")
+
+print(triangle_area(x, y, z))
+
+
+# 7.
+
+# 8.
--- B5/B5.15_farmerJohn.py
+++ B5/B5.15_farmerJohn.py
@@ -24,13 +24,20 @@ x = y = 0
 r = 50
 dist = 50
 
+while True:
+    try:
+        rect = int(input('Choose a size for the rectangle'))
+        break
+    except ValueError:
+        print("Please type a valid number code.\n")
+
 # S
 move(s, -50, 100)
 s.color('#D3D3D3')
 s.fillcolor('#D3D3D3')
 s.begin_fill()
 for _ in range(4):
-    s.forward(100)
+    s.forward(rect)
     s.right(90)
 s.end_fill()
 
@@ -44,15 +51,21 @@ a.end_fill()
 
 # B
 move(b, x+dist, y+dist)
+a.begin_fill()
 b.circle(r)
+a.end_fill()
 
 # C
 move(c, x-dist, y-dist)
+a.begin_fill()
 c.circle(r)
+a.end_fill()
 
 # D
 move(d, x+dist, y-dist)
+a.begin_fill()
 d.circle(r)
+a.end_fill()
 
 
 turtle.exitonclick()

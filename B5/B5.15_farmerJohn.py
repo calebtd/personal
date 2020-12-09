8994ef58a89655ecd2f3a261cfe1344fbaf4f242
Caleb <caleb@dillenbeck.org>
2020-12-09 04:31:12 -0700

    Farmer John Done

--- B5/B5.15_farmerJohn.py
+++ B5/B5.15_farmerJohn.py
@@ -1,71 +1,109 @@
 # Caleb Dillenbeck
 # B5 Programming 1 - Mr. Blair
-# Farmer John
+# UVU Project: Farmer John's Field
+# Due 12/3/20
 # I did not copy anyone
+
+# The problem:
+#   Draw the field with turtle graphics and calculate the unwatered area
+
+# Steps:
+#   Get input with nice exceptions, draw the square, draw the circles,
+#   calculate the area, print the results
+
+# What I learned:
+#   How to use try blocks better
+#   How to better implement functions and solve basic math neatly
+
 
 import turtle
 import math
 
 
-def move(t, h, v):
+def move(h, v):
+    # Function for moving the turtle easier
     t.pu()
     t.goto(h, v)
     t.pd()
 
 
-a = b = c = d = turtle.Turtle()
-# b = turtle.Turtle()
-# c = turtle.Turtle()
-# d = turtle.Turtle()
+def circles(a, b, r):
+    # Function for drawing the circles, calls move() and then draws
+    t.begin_fill()
+    move(a, b)
+    t.circle(r)
+    t.end_fill()
 
-s = turtle.Turtle()
 
-x = y = 0
-r = 50
-dist = 50
-
+# Try block for invalid input
 while True:
     try:
-        rect = int(input('Choose a size for the rectangle'))
-        break
+        # Get the input
+        side = int(input('Choose a size for the rectangle: '))
+        # raise the exception manually if negative
+        if side > 0:
+            break
+        else:
+            raise ValueError
+    # do this if there's an exception
     except ValueError:
         print("Please type a valid number code.\n")
 
-# S
-move(s, -50, 100)
-s.color('#D3D3D3')
-s.fillcolor('#D3D3D3')
-s.begin_fill()
+# These variables make things easier to deal with
+t = turtle.Turtle()
+radius = side/2
+
+# Make the square (with colors!)
+move((side/2), (side/2))
+t.color('#4F4E4F')
+t.fillcolor('#4F4E4F')
+t.begin_fill()
 for _ in range(4):
-    s.forward(rect)
-    s.right(90)
-s.end_fill()
+    t.right(90)
+    t.forward(side)
+t.end_fill()
 
+# Set colors back for the circles
+t.color('#000000')
+t.fillcolor('#FFFFFF')
 
-# A
-move(a, x-dist, y+dist)
-a.fillcolor('#FFFFFF')
-a.begin_fill()
-a.circle(r)
-a.end_fill()
+# First circle
+circles(-(side/2), 0, radius)
 
-# B
-move(b, x+dist, y+dist)
-a.begin_fill()
-b.circle(r)
-a.end_fill()
+# Second circle
+circles((side/2), 0, radius)
 
-# C
-move(c, x-dist, y-dist)
-a.begin_fill()
-c.circle(r)
-a.end_fill()
+# Third circle
+circles(-(side/2), -side, radius)
 
-# D
-move(d, x+dist, y-dist)
-a.begin_fill()
-d.circle(r)
-a.end_fill()
+# Fourth circle
+circles((side/2), -side, radius)
 
+# Redraw square on top
+t.color('#4F4E4F')
+move((side/2), (side/2))
+for _ in range(4):
+    t.right(90)
+    t.forward(side)
+# done with turtle, so hide it
+t.hideturtle()
 
-turtle.exitonclick()
+# ---Area Logic---
+# area of a square: b * h
+# area of a circle quadrant: πr² / 4
+
+# You can get the area of the unwatered section by
+# subtracting 4 quadrants from the square
+
+squareArea = side * side
+quadrantArea = (math.pi * (radius**2)) / 4
+unwateredArea = squareArea - (quadrantArea * 4)
+
+# OR simplified down
+
+notWatered = side**2 - (math.pi * (radius**2))
+
+# print the result, rounded to 2 decimal places
+print(f'The unwatered area is: {notWatered:.2f}')
+
+turtle.done()

1ea4c6744523f8281f360f19706532e61305ece8
Caleb <caleb@dillenbeck.org>
2021-02-05 07:26:44 -0700

    CSV Almost Done

--- B5/B5.18_csvFile.py
+++ B5/B5.18_csvFile.py
@@ -4,7 +4,44 @@
 # I did not copy anyone
 
 import csv
-with open('station2.csv', 'r') as file:
-    for row in csv.reader(file):
-        if row[7] != '':
-            print(row[7])
+
+# Problem 1
+with open('stations-filtered.csv', 'w', newline='') as filteredFile:
+    writer = csv.writer(filteredFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    with open('station2.csv', 'r') as stationFile:
+        for row in csv.reader(stationFile):
+            if row[7] != '':
+                writer.writerow(row)
+
+# Problem 2
+drainage = []
+with open('stations-filtered.csv', 'r', newline='') as filteredFile:
+    reader = csv.reader(filteredFile)
+    next(reader)
+    for row in reader:
+        drainage.append(float(row[7]))
+
+with open('drainage-area-avg.txt', 'w') as drainageAverage:
+    x = 0
+    y = len(drainage)
+    for value in drainage:
+        x += value
+    result = x / y
+    drainageAverage.write(str(result))
+    # print(f'{result:.4f}')
+
+# Problem 3
+
+# ord('')
+# chr()
+
+# fileName = input("What's the input file name?: ")
+# shift = input("How many shift values?: ")
+
+fileName = 'text.txt'
+shift = 5
+
+with open(f'{fileName}.txt') as cleanFile:
+    for word in cleanFile:
+        for letter in word:
+            pass

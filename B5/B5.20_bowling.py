17ee5af31d5db7ac2d3df4fd1814ee52435b32c3
Caleb <caleb@dillenbeck.org>
2021-02-11 22:34:53 -0700

    Bowling almost done

--- B5/B5.20_bowling.py
+++ B5/B5.20_bowling.py
@@ -2,3 +2,71 @@
 # B5 Programming 1 - Mr. Blair
 # Bowling Team
 # I did not copy anyone
+
+
+# Function for printing lists nicely
+def printTable(dictionary, sorter=False):  # Pass the dictionary, pass a sorter if there is one
+    # Newline
+    print()
+    # Get the length of the longest dictionary key, use that to space the printed list
+    longest_key = max(map(len, dictionary))
+
+    # Format the list if there's an asterisk
+    for keys, values in dictionary.items():
+        if values == 300:
+            longest_key += 1
+
+    if not sorter:
+        for keys, values in dictionary.items():
+            if values == 300:
+                keys = f'*{keys}'
+            print(f'{keys:{longest_key}} - {values}')
+    else:
+        # Use a lambda to choose which thing to sort by
+        for keys, values in sorted(dictionary.items(), key=lambda x: x[sorter]):
+            if values == 300:
+                keys = f'*{keys}'
+            print(f'{keys:{longest_key}} - {values}')
+
+
+# Establish dictionary
+scores = {}
+
+# Loop to get all names and scores
+while True:
+    splitter = input("Name and score: ").split()
+
+    # When it gets a blank line, break
+    if not splitter:
+        break
+    else:
+        # Check for a perfect game
+        # if splitter[-1] == '300':
+        #     splitter[0] = f'*{splitter[0]}'
+
+        # Turn the score into an int
+        try:
+            score_int = int(splitter[-1])
+            scores[splitter[0]] = score_int
+        except ValueError:
+            print("Make sure all scores are numbers.")
+
+# Print in input order
+printTable(scores)
+
+# Print in numerical order
+printTable(scores, 1)  # The 1 is for the lambda, sort by the value index
+
+# Print in alphabetical order
+printTable(scores, 0)  # The 0 is for the lambda, sort by the key index
+
+
+# Get the key for max and min values
+maxKey = max(scores, key=scores.get)
+minKey = min(scores, key=scores.get)
+
+print(f"\nCongrats to {maxKey} for a high score of {scores[maxKey]}!")
+print(f"Better luck next time {minKey}, with {scores[minKey]}.")
+
+with open('game_results.txt', 'w') as resultsFile:
+    pass

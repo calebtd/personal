0268561f1c5a6e330122ee001bfcbf4c7f107bc4
Caleb <caleb@dillenbeck.org>
2021-03-03 18:54:36 -0700

    Bowling Done

--- B5/B5.20_bowling.py
+++ B5/B5.20_bowling.py
@@ -1,83 +1,90 @@
 # Caleb Dillenbeck
 # B5 Programming 1 - Mr. Blair
-# Bowling Team
-# I did not copy anyone
+# Bowling Team - Feb 26
+
+# The problem is to get data from a bowling
+# game, sort it, and print it back
+
+# I learned to take a step back and don't
+# make it harder than it needs to be
 
 
-# Function for printing lists nicely
-def print_table(dictionary, sorter=False):  # Pass the dictionary, pass a sorter if there is one
-    # Newline
-    print()
-    # Get the length of the longest dictionary key, use that to space the printed list
-    longest_key = max(map(len, dictionary))
-
-    # Format the list if there's an asterisk
-    for values in dictionary.values():
-        if values == 300:
-            longest_key += 1
-
-    if not sorter:
-        for keys, values in dictionary.items():
-            if values == 300:
-                keys = f'*{keys}'
-            print(f'{keys:{longest_key}} - {values}')
-    else:
-        pass
-
-    print()
-    for w in sorted(dictionary, key=dictionary.get, reverse=True):
-        print(f'{w:{longest_key}} - {dictionary[w]}')
-
-    print()
-    for w in sorted(dictionary):
-        print(w, dictionary[w])
+# This function prints each line to the screen and saves
+# the line to a list, eventually going into the file
+def printout(to_print):
+    global output
+    output.append(f'{to_print}\n')
+    print(to_print)
 
 
-# Establish dictionary
+# Establish dictionary/list
 scores = {}
+output = []
 
 # Loop to get all names and scores
 while True:
-    splitter = input("Name and score: ").split()
+    splitter = input("First name and score: ").split()
 
     # When it gets a blank line, break
     if not splitter:
+        print()
         break
     else:
-        # Check for a perfect game
-        # if splitter[-1] == '300':
-        #     splitter[0] = f'*{splitter[0]}'
-
-        # Turn the score into an int
+        # Turn the score into an int and add it to the dictionary
         try:
             score_int = int(splitter[-1])
             scores[splitter[0]] = score_int
         except ValueError:
             print("Make sure all scores are numbers.")
 
-# Print in input order
-print_table(scores)
+# Create a new dictionary for the asterisks
+scoresAsterisk = {}
 
+for key, value in scores.items():
+    if value == 300:
+        key = '*'+key  # Add asterisk
+    scoresAsterisk[key] = value
 
-# Print in numerical order
-# print_table(scores, 1)  # The 1 is for the lambda, sort by the value index
+# Check for longest string in the keys to format properly
+longest_key = max(map(len, scoresAsterisk))
 
-# Print in alphabetical order
-# print_table(scores, 0)  # The 0 is for the lambda, sort by the key index
+# Each of these uses the printout function
+# to go to the screen AND the file
 
+printout('--Order entered--')
+for key, value in scoresAsterisk.items():
+    printout(f'{key:{longest_key}} - {value}')
 
-# Get the key for max and min values
+printout('\n--High score--')
+for key in sorted(scoresAsterisk, key=scoresAsterisk.get, reverse=True):
+    printout(f'{key:{longest_key}} - {scoresAsterisk[key]}')
+
+# This one is special because it needs to sort
+# without the asterisk and then add it back in
+printout('\n--Alphabetical--')
+for key in sorted(scores):
+    if scores[key] == 300:
+        newKey = '*'+key
+    else:
+        newKey = key
+    printout(f'{newKey:{longest_key}} - {scores[key]}')
+
+# Get the max and min scores
 maxKey = max(scores, key=scores.get)
 minKey = min(scores, key=scores.get)
 
-print(f"\nCongrats to {maxKey} for a high score of {scores[maxKey]}!")
-print(f"Better luck next time {minKey}, with {scores[minKey]}.")
+# Display messages
+printout(f"\nCongrats to {maxKey} for a high score of {scores[maxKey]}!")
+printout(f"Better luck next time, {minKey}, with {scores[minKey]}.")
 
+# Calculate average
 addedScore = 0
 for val in scores.values():
     addedScore += val
 averageScore = addedScore // len(scores)
-print(f"The team average score is {averageScore}.")
+printout(f"The team average score is {averageScore}.")
 
+# Write each line to the file
 with open('game_results.txt', 'w') as resultsFile:
-    pass
+    for line in output:
+        resultsFile.write(line)
